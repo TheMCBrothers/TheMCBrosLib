3351134d8ca1e202290ffc6cf34fa67d8e0dc147
TheMCLoveMan <themcloveman@gmail.com>
2023-01-22 20:56:23 +0100

    add energy utils

---
+++ src/main/java/net/themcbrothers/lib/util/EnergyUtils.java
@@ -0,0 +1,32 @@
+package net.themcbrothers.lib.util;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
+import net.minecraftforge.energy.IEnergyStorage;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Helps with {@link IEnergyStorage}
+ */
+public class EnergyUtils {
+    /**
+     * Get an energy storage from a block entity at a specific pos
+     *
+     * @param level Level
+     * @param pos   Block pos
+     * @param side  Optional side
+     * @return Energy Storage
+     */
+    public static IEnergyStorage getEnergy(Level level, BlockPos pos, @Nullable Direction side) {
+        BlockEntity blockEntity = level.getBlockEntity(pos);
+
+        if (blockEntity != null) {
+            return blockEntity.getCapability(ForgeCapabilities.ENERGY, side).map(energyStorage -> energyStorage).orElse(null);
+        }
+
+        return null;
+    }
+}
