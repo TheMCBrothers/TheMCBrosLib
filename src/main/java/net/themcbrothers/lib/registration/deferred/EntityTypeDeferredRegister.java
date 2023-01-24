d5dfb32a30d50f44b00799275f72a251024a5ba9
TheMCLoveMan <themcloveman@gmail.com>
2023-01-25 00:32:08 +0100

    add EntityTypeDeferredRegister

---
+++ src/main/java/net/themcbrothers/lib/registration/deferred/EntityTypeDeferredRegister.java
@@ -0,0 +1,59 @@
+package net.themcbrothers.lib.registration.deferred;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.common.ForgeSpawnEggItem;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
+
+/**
+ * Deferred register for an entity, building the type from a builder instance and adding an egg
+ */
+public class EntityTypeDeferredRegister extends DeferredRegisterWrapper<EntityType<?>> {
+    private final DeferredRegister<Item> itemRegister;
+
+    public EntityTypeDeferredRegister(String modId) {
+        super(Registries.ENTITY_TYPE, modId);
+        this.itemRegister = DeferredRegister.create(Registries.ITEM, modId);
+    }
+
+    @Override
+    public void register(IEventBus bus) {
+        super.register(bus);
+        this.itemRegister.register(bus);
+    }
+
+    /**
+     * Registers an entity type for the given entity type builder
+     *
+     * @param name Entity name
+     * @param sup  Entity builder instance
+     * @param <T>  Entity class type
+     * @return Entity registry object
+     */
+    public <T extends Entity> RegistryObject<EntityType<T>> register(String name, Supplier<EntityType.Builder<T>> sup) {
+        return register.register(name, () -> sup.get().build(resourceName(name)));
+    }
+
+    /**
+     * Registers an entity type for the given entity type builder, and registers a spawn egg for it
+     *
+     * @param name      Entity name
+     * @param sup       Entity builder instance
+     * @param primary   Primary egg color
+     * @param secondary Secondary egg color
+     * @param <T>       Entity class type
+     * @return Entity registry object
+     */
+    public <T extends Mob> RegistryObject<EntityType<T>> registerWithEgg(String name, Supplier<EntityType.Builder<T>> sup, int primary, int secondary) {
+        RegistryObject<EntityType<T>> object = register(name, sup);
+        itemRegister.register(name + "_spawn_egg", () -> new ForgeSpawnEggItem(object, primary, secondary, new Item.Properties()));
+        return object;
+    }
+}
