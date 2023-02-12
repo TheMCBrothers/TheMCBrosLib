6e63320c119dd615f6431dfd37abca19843121a1
TheMCLoveMan <themcloveman@gmail.com>
2023-02-12 15:46:12 +0100

    make it unable to tab on screen widgets

--- src/main/java/net/themcbrothers/lib/client/screen/widgets/FluidTank.java
+++ src/main/java/net/themcbrothers/lib/client/screen/widgets/FluidTank.java
@@ -152,4 +152,9 @@ public class FluidTank extends AbstractWidget {
     @Override
     public void updateNarration(NarrationElementOutput narrationElementOutput) {
     }
+
+    @Override
+    public boolean changeFocus(boolean focus) {
+        return false;
+    }
 }
