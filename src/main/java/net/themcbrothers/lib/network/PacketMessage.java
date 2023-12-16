da2cdb92fe442db1c73bc24db2434e7aa27b783d
TheMCLoveMan <themcloveman@gmail.com>
2023-12-16 07:26:16 +0100

    update PacketMessage

--- src/main/java/net/themcbrothers/lib/network/PacketMessage.java
+++ src/main/java/net/themcbrothers/lib/network/PacketMessage.java
@@ -2,10 +2,9 @@ package net.themcbrothers.lib.network;
 
 import net.minecraft.network.FriendlyByteBuf;
 import net.neoforged.neoforge.network.NetworkEvent;
-import java.util.function.Supplier;
 
 public interface PacketMessage {
     void toBytes(FriendlyByteBuf buffer);
 
-    void process(Supplier<NetworkEvent.Context> context);
+    void process(NetworkEvent.Context context);
 }
