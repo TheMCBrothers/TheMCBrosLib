274288ff5fe67d9e97c75502a75457bd65b8b3a7
TheMCLoveMan <themcloveman@gmail.com>
2023-12-27 17:55:02 +0100

    generated data 1.20.4

--- src/main/java/net/themcbrothers/lib/datagen/DataGeneration.java
+++ src/main/java/net/themcbrothers/lib/datagen/DataGeneration.java
@@ -1,23 +1,15 @@
 package net.themcbrothers.lib.datagen;
 
-import net.minecraft.DetectedVersion;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
-import net.minecraft.data.metadata.PackMetadataGenerator;
-import net.minecraft.network.chat.Component;
-import net.minecraft.server.packs.PackType;
-import net.minecraft.server.packs.metadata.pack.PackMetadataSection;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 import net.themcbrothers.lib.TheMCBrosLib;
 
-import java.util.Arrays;
 import java.util.concurrent.CompletableFuture;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 @Mod.EventBusSubscriber(modid = TheMCBrosLib.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class DataGeneration {
@@ -30,11 +22,5 @@ public class DataGeneration {
 
         generator.addProvider(event.includeServer(), new LibraryTagsProvider.Blocks(packOutput, lookupProvider, existingFileHelper));
         generator.addProvider(event.includeServer(), new LibraryRecipeProvider(packOutput, lookupProvider));
-
-        // pack.mcmeta
-        generator.addProvider(true, new PackMetadataGenerator(packOutput))
-                .add(PackMetadataSection.TYPE, new PackMetadataSection(Component.literal("TheMCBrosLib"),
-                        DetectedVersion.BUILT_IN.getPackVersion(PackType.CLIENT_RESOURCES), Arrays.stream(PackType.values())
-                        .collect(Collectors.toMap(Function.identity(), DetectedVersion.BUILT_IN::getPackVersion))));
     }
 }
