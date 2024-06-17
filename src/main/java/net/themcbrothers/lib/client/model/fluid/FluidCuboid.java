2d75117cf60e8e25fae265a6d49245ec9892c360
TheMCLoveMan <themcloveman@gmail.com>
2024-06-17 22:38:42 +0200

    remove unused methods

--- src/main/java/net/themcbrothers/lib/client/model/fluid/FluidCuboid.java
+++ src/main/java/net/themcbrothers/lib/client/model/fluid/FluidCuboid.java
@@ -1,19 +1,11 @@
 package net.themcbrothers.lib.client.model.fluid;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonSyntaxException;
 import net.minecraft.core.Direction;
-import net.minecraft.util.GsonHelper;
-import net.themcbrothers.lib.util.JsonHelper;
-import net.themcbrothers.lib.util.ModelHelper;
 import org.joml.Vector3f;
+
 import javax.annotation.Nullable;
-import java.util.Collections;
 import java.util.EnumMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
 public class FluidCuboid {
     public static final Map<Direction, FluidFace> DEFAULT_FACES;
@@ -92,72 +84,6 @@ public class FluidCuboid {
             toScaled.mul(1 / 16f);
         }
         return toScaled;
-    }
-
-    /**
-     * Creates a new fluid cuboid from JSON
-     *
-     * @param json JSON object
-     * @return Fluid cuboid
-     */
-    public static FluidCuboid fromJson(JsonObject json) {
-        Vector3f from = ModelHelper.arrayToVector(json, "from");
-        Vector3f to = ModelHelper.arrayToVector(json, "to");
-        // if faces is defined, fill with specified faces
-        Map<Direction, FluidFace> faces = getFaces(json);
-        return new FluidCuboid(from, to, faces);
-    }
-
-    /**
-     * Gets a list of fluid cuboids from the given parent
-     *
-     * @param parent Parent JSON
-     * @param key    List key
-     * @return List of cuboids
-     */
-    public static List<FluidCuboid> listFromJson(JsonObject parent, String key) {
-        JsonElement json = parent.get(key);
-
-        // object: one cube
-        if (json.isJsonObject()) {
-            return Collections.singletonList(fromJson(json.getAsJsonObject()));
-        }
-
-        // array: multiple cubes
-        if (json.isJsonArray()) {
-            return JsonHelper.parseList(json.getAsJsonArray(), key, FluidCuboid::fromJson);
-        }
-
-        throw new JsonSyntaxException("Invalid fluid '" + key + "', must be an array or an object");
-    }
-
-    /**
-     * Gets a face set from the given json element
-     *
-     * @param json JSON parent
-     * @return Map of faces
-     */
-    protected static Map<Direction, FluidFace> getFaces(JsonObject json) {
-        if (!json.has("faces")) {
-            return DEFAULT_FACES;
-        }
-
-        Map<Direction, FluidFace> faces = new EnumMap<>(Direction.class);
-        JsonObject object = GsonHelper.getAsJsonObject(json, "faces");
-        for (Entry<String, JsonElement> entry : object.entrySet()) {
-            // if the direction is a face, add it
-            String name = entry.getKey();
-            Direction dir = Direction.byName(name);
-            if (dir != null) {
-                JsonObject face = GsonHelper.convertToJsonObject(entry.getValue(), name);
-                boolean flowing = GsonHelper.getAsBoolean(face, "flowing", false);
-                int rotation = ModelHelper.getRotation(face, "rotation");
-                faces.put(dir, new FluidFace(flowing, rotation));
-            } else {
-                throw new JsonSyntaxException("Unknown face '" + name + "'");
-            }
-        }
-        return faces;
     }
 
     public Vector3f getFrom() {
