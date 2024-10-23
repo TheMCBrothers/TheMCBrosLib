4112caa6e12ce9d1c58625cd89a01cc43ee488d2
TheMCLoveMan <themcloveman@gmail.com>
2024-10-23 18:52:27 +0200

    add EnergyContainerItem#getEnergyStored

--- src/main/java/net/themcbrothers/lib/energy/EnergyContainerItem.java
+++ src/main/java/net/themcbrothers/lib/energy/EnergyContainerItem.java
@@ -1,11 +1,21 @@
 package net.themcbrothers.lib.energy;
 
+import net.minecraft.world.item.ItemStack;
+import net.themcbrothers.lib.LibDataComponents;
+
 /**
  * Implement this interface on Item classes that support external manipulation of their internal energy storages.
  * <p>
  * Example {@link BasicEnergyContainerItem}
  */
 public interface EnergyContainerItem {
+    /**
+     * Returns the energy stored in the container
+     */
+    default int getEnergyStored(ItemStack container) {
+        return container.getOrDefault(LibDataComponents.ENERGY, 0);
+    }
+
     /**
      * Returns the capacity
      */
